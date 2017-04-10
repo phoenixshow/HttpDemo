f1c6fc2d51fc174cfa0465dd37486f2f1fc2d51b
phoenixshow <965550123@qq.com>
2017-04-11 00:29:07 +0800

    DownloadDemo、UploadDemo添加了6.0动态授权

--- DownloadDemo/app/src/main/java/com/phoenix/download/MainActivity.java
+++ DownloadDemo/app/src/main/java/com/phoenix/download/MainActivity.java
@@ -1,30 +1,86 @@
 package com.phoenix.download;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean isGrant = true;
+    private final int SDK_PERMISSION_REQUEST = 127;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getPersimmions();
     }
 
     public void downloadImg(View view) {
-        Intent intent = new Intent(this, ImageActivity.class);
-        startActivity(intent);
+        if (isGrant) {
+            Intent intent = new Intent(this, ImageActivity.class);
+            startActivity(intent);
+        }else {
+            Toast.makeText(this,getString(R.string.permisstion_deny),Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void downloadHtml(View view) {
-        Intent intent = new Intent(this, HtmlActivity.class);
-        startActivity(intent);
+        if (isGrant) {
+            Intent intent = new Intent(this, HtmlActivity.class);
+            startActivity(intent);
+        }else {
+            Toast.makeText(this,getString(R.string.permisstion_deny),Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void downloadFile(View view) {
-        Intent intent = new Intent(this, FileActivity.class);
-        startActivity(intent);
+        if (isGrant) {
+            Intent intent = new Intent(this, FileActivity.class);
+            startActivity(intent);
+        }else {
+            Toast.makeText(this,getString(R.string.permisstion_deny),Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @TargetApi(23)
+    private void getPersimmions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<>();
+            if(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @TargetApi(23)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case SDK_PERMISSION_REQUEST:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    // 允许
+                    Toast.makeText(this,getString(R.string.permisstion_grant),Toast.LENGTH_SHORT).show();
+                }else{
+                    // 不允许
+                    isGrant = false;
+                    Toast.makeText(this,getString(R.string.permisstion_deny),Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
--- UploadDemo/app/src/main/java/com/phoenix/upload/MainActivity.java
+++ UploadDemo/app/src/main/java/com/phoenix/upload/MainActivity.java
@@ -1,21 +1,29 @@
 package com.phoenix.upload;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private boolean isGrant = true;
+    private final int SDK_PERMISSION_REQUEST = 127;
 
     private Button btn1;
     private Button btn2;
 
-    private String uploadUrl = "http://192.168.155.104:8080/web/servlet/UploadFileServlet";
+    private String uploadUrl = "http://192.168.0.104:8080/web/servlet/UploadFileServlet";
     private String filePath;
 
     @Override
@@ -28,21 +36,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
+
+        getPersimmions();
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn1:
-                chooseFile();
+                if (isGrant) {
+                    chooseFile();
+                }else {
+                    Toast.makeText(this,getString(R.string.permisstion_deny),Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.btn2:
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        UploadUtil.uploadFile(new File(filePath), uploadUrl);
-                    }
-                }).start();
+                if (isGrant) {
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            UploadUtil.uploadFile(new File(filePath), uploadUrl);
+                        }
+                    }).start();
+                }else {
+                    Toast.makeText(this,getString(R.string.permisstion_deny),Toast.LENGTH_SHORT).show();
+                }
                 break;
             default:
                 break;
@@ -69,5 +87,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
         }
         super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @TargetApi(23)
+    private void getPersimmions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<>();
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+            }
+
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @TargetApi(23)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case SDK_PERMISSION_REQUEST:
+                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    // 允许
+                    Toast.makeText(this,getString(R.string.permisstion_grant),Toast.LENGTH_SHORT).show();
+                }else{
+                    // 不允许
+                    isGrant = false;
+                    Toast.makeText(this,getString(R.string.permisstion_deny),Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
